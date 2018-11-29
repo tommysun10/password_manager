2d925b2c9b0019fd5e798b8d7475eb5ad0964a45
tommysun10 <tommysun10@hotmail.com>
2018-11-29 00:22:45 -0500

    Added basic navigation to navbar

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -5,6 +5,8 @@ export default class NavBar extends React.Component {
     render() {
         return (
                 <div>
+                    <Link to='/'> Home | </Link>
+                    <Link to='/profile'>Profile | </Link>
                     <Link to='/login'>Login</Link>
                     <Link to='/register'> | Register</Link>
                 </div>
